283e0020cdb4b388d6c28941baed65db67a0dd4b
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2024-01-11 12:35:47 +0300

    Add class Laser for Enemy.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Scripts
 {
-    public class Enemy : MonoBehaviour { }
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] private Laser _laserPrefab;
+        [SerializeField] private Transform _shootPosition;
+        
+        private Coroutine _fireCoroutine;
+        
+        private void OnEnable() => 
+            _fireCoroutine = StartCoroutine(Fire());
+
+        private void OnDisable()
+        {
+            if (_fireCoroutine != null)
+                StopCoroutine(_fireCoroutine);
+        }
+
+        private IEnumerator Fire()
+        {
+            float minDelay = 0.5f;
+            float maxDelay = 1.5f;
+            
+            while (true)
+            {
+                float randomDelay = Random.Range(minDelay, maxDelay);
+                yield return new WaitForSeconds(randomDelay);
+                
+                Instantiate(_laserPrefab, _shootPosition);
+            }
+        }
+    }
 }
